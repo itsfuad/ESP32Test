f782971791adb03baee0436f21e3091807efea69
Fuad Hasan <fuad.cs22@gmail.com>
2025-08-01 01:23:58 +0600

    Delete build directory

--- build/core/USB.cpp.d
+++
@@ -1,5 +0,0 @@
-D:\dev\ESP32\ESP32Test\build\core\USB.cpp.o: \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.0.7\cores\esp32\USB.cpp \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.0.7\cores\esp32\USB.h \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.1-632e0c2a\esp32/include/soc/esp32/include/soc/soc_caps.h \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\tools\esp32-arduino-libs\idf-release_v5.1-632e0c2a\esp32/qio_qspi/include/sdkconfig.h
--- build/core/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-D:\dev\ESP32\ESP32Test\build\core\firmware_msc_fat.c.o: \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.0.7\cores\esp32\firmware_msc_fat.c \
- C:\Users\Fuad\ Hasan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.0.7\cores\esp32\firmware_msc_fat.h
